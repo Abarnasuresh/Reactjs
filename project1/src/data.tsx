c09f4bfb5b28b15772a93087b5f83864cd7b106a
Abarna <abarnasuresh20012gmail.com>
2022-06-06 17:11:33 +0530

    router

--- project1/src/data.tsx
+++ project1/src/data.tsx
@@ -10,20 +10,20 @@ export default [
             reviewCount: 6
         },
         location: "Online",
-        openSpots: 0,
+        openSpots: 0, 
     }, 
     { 
         id: 2,
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-        price: 125, 
+        price: 125,
         coverImg: "wedding-photography.png",
         stats: {
             rating: 5.0,
             reviewCount: 30
         },
         location: "Online",
-        openSpots: 27,
+        openSpots: 27, 
     },
     {
         id: 3,
